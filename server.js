e3bff7a5f892961d2911b047d5770e6d66d1bb03
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-03 21:51:51 -0400

    Final

--- server.js
+++ server.js
@@ -24,15 +24,17 @@ app.use((err, req , res, next)=>{
     res.status(500).json({error: 'Internal server error'});
 });
 
-//This is a function asincrona, waiting for response 
-const startServer = async() => {
-    await initDB(); 
-    await connectRabbit();
+const startServer = async () => {
+    try {
+        await initDB();
+        await connectRabbit();
+    } catch (error) {
+        console.error('Error al iniciar:', error.message);
+    }
+
     const PORT = process.env.PORT || 3002;
-    app.listen(PORT ,()=>{
-        console.log(`Report running in port ${PORT}`);
-  
-        
+    app.listen(PORT, () => {
+        console.log(`Report service running on port ${PORT}`);
     });
 };
 
