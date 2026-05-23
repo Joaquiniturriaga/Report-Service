0e6efa783a3dad9f96bc2950d1aa863943026808
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-22 20:48:35 -0400

    (feat): ID-2-REPORT-SERVICE: Implementacion de update

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createReport, getReports } = require('../controllers/report.controller');
+const { createReport, getReports, updateReportStatus } = require('../controllers/report.controller');
 const { validateToken } = require('../middleware/validateToken.middleware');
+const { authorizeRole } = require('../middleware/roles.middleware');
 
 const router = express.Router();
 
-router.post('/', validateToken, createReport);
-router.get('/', validateToken, getReports);
-
+router.post('/',           validateToken,                         createReport);
+router.get('/',            validateToken,                         getReports);
+router.put('/:id/status',  validateToken, authorizeRole('admin'), updateReportStatus);
+ 
 module.exports = router;
