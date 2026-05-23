1100c1476f10495f87981d63e0e21d1a575a2a0e
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-22 21:08:15 -0400

    (feat): ID-3-REPORT-SERVICE: DEPENDENCIAS

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const { createReport, getReports, updateReportStatus } = require('../controllers/report.controller');
 const { validateToken } = require('../middleware/validateToken.middleware');
-const { authorizeRole } = require('../middleware/roles.middleware');
 
 const router = express.Router();
 
 router.post('/',           validateToken,                         createReport);
 router.get('/',            validateToken,                         getReports);
-router.put('/:id/status',  validateToken, authorizeRole('admin'), updateReportStatus);
+router.put('/:id/status',  validateToken, updateReportStatus);
  
 module.exports = router;
