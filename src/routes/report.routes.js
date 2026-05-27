895ee9509beb26e8b63779ceb172814e4a907850
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-27 19:52:14 -0400

    (fix): ID-5-REPORT-SERVICE:Liquidacion de middleware

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const { createReport, getReports, updateReportStatus } = require('../controllers/report.controller');
-const { validateToken } = require('../middleware/validateToken.middleware');
+const { validateToken }          = require('../middleware/validateToken.middleware');
 const { validateInternalSecret } = require('../middleware/internalSecret.middleware');
-const { validateReportBody } = require('../middleware/validateReport.middleware');
+const { validateReport }         = require('../middleware/validateReport.middleware');
 
 const router = express.Router();
-//Rutas normales pasan por validateToken( y el rate limit aplicado del server)
+
 router.put('/internal/:id/status', validateInternalSecret, updateReportStatus);
 
-router.post('/',validateToken,  validateReportBody,createReport);
-router.get('/', validateToken ,getReports);
-router.put('/:id/status', validateToken, updateReportStatus);  
-
-
-router.put('/:id/status', validateToken,updateReportStatus);
+router.post('/',          validateToken, validateReport,  createReport);
+router.get('/',           validateToken,                  getReports);
+router.put('/:id/status', validateToken,                  updateReportStatus);
 
 module.exports = router;
